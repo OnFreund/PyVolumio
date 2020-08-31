852871b79e88d211e8861d1cb98902254168499f
On Freund <onfreund@gmail.com>
2020-08-31 20:45:19 +0300

    0.1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/OnFreund/PyVolumio'
 EMAIL = 'onfreund@gmail.com'
 AUTHOR = 'On Freund'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 
 REQUIRED = ['aiohttp']
 
